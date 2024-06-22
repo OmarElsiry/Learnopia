dba70a9a3a596cbcec6a4d20c18a4cb3757ca2e7
Potter_Parker <Omarelsiry010@gmail.com>
2024-06-23 00:07:14 +0300

    err handling: robot not found

--- lib/features/communicate_robot/send_message_flask.dart
+++ lib/features/communicate_robot/send_message_flask.dart
@@ -1,22 +1,31 @@
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void sendMessageToRobotFlask(String message) async {
   var url = Uri.parse('http://172.20.10.6:51000/print_message');
-  var response = await http.post(
-    url,
-    headers: {"Content-Type": "application/json"},
-    body: json.encode({"message": message}),
-  );
 
-  if (response.statusCode == 200) {
-    if (kDebugMode) {
-      print('Message sent successfully to the robot');
+  try {
+    var response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: json.encode({"message": message}),
+    );
+
+    if (response.statusCode == 200) {
+      if (kDebugMode) {
+        print('Message sent successfully to the robot');
+      }
+    } else {
+      if (kDebugMode) {
+        print('Failed to send message to the robot');
+      }
     }
-  } else {
-    if (kDebugMode) {
-      print('Failed to send message to the robot');
-    }
+  } catch (_) {
+    // Wait for 5 seconds before showing the snack bar
+    Future.delayed(const Duration(seconds: 5), () {
+      print("robot not found $e");
+    });
   }
 }
